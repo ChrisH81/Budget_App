91aa446077920c06ad99f5d20e3bec3ba992e53f
ChrisH81 <chrishurst1981@icloud.com>
2017-12-15 10:52:59 +0000

    add eventListener for keypresses

--- app.js
+++ app.js
@@ -16,6 +16,28 @@ var UIController = (function() {
 //  Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
 
+  var ctrlAddItem = function() {
 
+    // 1. Get filled input data
+
+    // 2. Add tje item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget__title
+
+    // 5. Display the budget on the UI
+    
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event){
+
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+
+  });
 
 })(budgetController, UIController);
